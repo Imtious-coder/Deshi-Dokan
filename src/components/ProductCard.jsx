7e24adc560fba9331fead81001c921c5156f2c2c
Imtious-coder <iarafat386@gmail.com>
2022-02-20 18:58:29 +0600

    Banner Updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,4 @@
 import {Image, Link} from '@shopify/hydrogen';
-
 import MoneyCompareAtPrice from './MoneyCompareAtPrice.client';
 import MoneyPrice from './MoneyPrice.client';
 
@@ -16,7 +15,7 @@ export default function ProductCard({product}) {
   return (
     <div className="text-md mb-4 relative">
       <Link to={`/products/${product.handle}`}>
-        <div className="rounded-lg border-2 border-gray-200 mb-2 relative flex items-center justify-center overflow-hidden object-cover h-96">
+        <div className="rounded-lg border-2 border-gray-200 mb-2 relative flex items-center justify-center overflow-hidden object-cover h-60">
           {selectedVariant.image ? (
             <Image
               className="bg-white absolute w-full h-full transition-all duration-500 ease-in-out transform bg-center bg-cover object-center object-contain hover:scale-110"
@@ -30,7 +29,7 @@ export default function ProductCard({product}) {
           )}
         </div>
 
-        <span className="text-black font-semibold mb-0.5">{product.title}</span>
+        <span className="text-black font-bold my-2">{product.title}</span>
 
         {product.vendor && (
           <p className="text-gray-900 font-medium text-sm mb-0.5">
